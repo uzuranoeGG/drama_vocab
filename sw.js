777449db4cf8c2590a378cdf951fdc51a038c60b
uzuranoeGG <167547114+uzuranoeGG@users.noreply.github.com>
2026-05-16 14:20:21 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,51 @@
-const CACHE = 'drama-vocab-v1';
-const ASSETS = ['./index.html', './manifest.json'];
+const CACHE = 'drama-vocab-free-v1';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+
+  e.waitUntil(
+    caches.keys().then(keys => {
+
+      return Promise.all(
+        keys
+          .filter(k => k !== CACHE)
+          .map(k => caches.delete(k))
+      );
+
+    })
+  );
+
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  if (e.request.url.includes('api.anthropic.com')) return;
+
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
+
+    caches.match(e.request).then(res => {
+
+      return (
+        res ||
+        fetch(e.request).catch(() => caches.match('./index.html'))
+      );
+
+    })
+
   );
 });
